0572b3e701b3856971a81554d657a66938216348
Liu Chong <lc87624@gmail.com>
2011-11-09 17:59:42 +0800

    first page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,8 +10,17 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
-        System.out.println("Yep!");
-    	render();
+        Post frontPost = Post.find("order by postedAt desc").first();
+        List<Post> olderPosts = Post.find(
+        			"order by postedAt desc"
+        		).from(1).fetch(10);
+        render(frontPost, olderPosts);
+    }
+    
+    @Before
+    public static void addDefaults(){
+    	renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+    	renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
     }
 
 }
