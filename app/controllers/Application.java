1a9c5bc801883f73851e54a5eaf3f90753349cdf
Liu Chong <lc87624@gmail.com>
2011-11-10 18:13:42 +0800

    add comment posting

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,10 @@
 package controllers;
 
 import play.*;
+import play.cache.Cache;
+import play.data.validation.Required;
+import play.libs.Codec;
+import play.libs.Images;
 import play.mvc.*;
 
 import java.util.*;
@@ -19,7 +23,25 @@ public class Application extends Controller {
     
     public static void show(Long id){
     	Post post = Post.findById(id);
-    	render(post);
+    	String randomID  = Codec.UUID();
+    	render(post, randomID);
+    }
+    
+    public static void postComment(Long postId, @Required String author, @Required String content){
+    	Post post = Post.findById(postId);
+    	if(validation.hasErrors()){
+    		render("Application/show.html", post);
+    	}
+    	post.addComment(author, content);
+    	flash.success("Thanks for posting, %s", author);
+    	render("Application/show.html", post);
+    }
+    
+    public static void captcha(String id){
+    	Images.Captcha captcha = Images.captcha();
+    	String code = captcha.getText("#E4EAFD");
+    	Cache.set(id, code, "10mn");
+    	renderBinary(captcha);
     }
     
     @Before
---
+++ app/models/Tag.java
@@ -0,0 +1,11 @@
+package models;
+
+import javax.persistence.Entity;
+
+import play.db.jpa.Model;
+
+@Entity
+public class Tag extends Model {
+	
+	
+}
